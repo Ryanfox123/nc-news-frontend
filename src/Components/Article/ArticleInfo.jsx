d92039894c38955ae835f97fe96fc43fa8bb1e87
Ryan Fox <ryanfox1@hotmail.co.uk>
2024-11-04 17:13:00 +0000

    article votes functionality

--- src/Components/Article/ArticleInfo.jsx
+++ src/Components/Article/ArticleInfo.jsx
@@ -2,7 +2,11 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import CommentCard from "./CommentCard";
 import { format } from "date-fns";
-import { getArticleByID, getCommentsByID } from "../../../utils";
+import {
+  getArticleByID,
+  getCommentsByID,
+  patchArticleVotes,
+} from "../../../utils";
 
 export default function ArticleInfo() {
   const { article_id } = useParams();
@@ -10,8 +14,33 @@ export default function ArticleInfo() {
   const [comments, setComments] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [upvote, setUpvote] = useState(false);
+  const [downvote, setDownvote] = useState(false);
+  const [commentError, setCommentError] = useState(null);
 
-  console.log(currArticle);
+  const handleUpvote = () => {
+    //votechange will be 1 if neither buttons are toggle, 2 if downvote is already toggled or -1 if i untoggle upvote. this is then passed into my api for patch method.
+    const voteChange = upvote ? -1 : downvote ? 2 : 1;
+    setUpvote(!upvote);
+    setDownvote(false);
+    setCurrArticle({ ...currArticle, votes: currArticle.votes + voteChange });
+
+    patchArticleVotes(article_id, voteChange).catch((err) => {
+      setCurrArticle({ ...currArticle, votes: currArticle.votes - voteChange });
+      setCommentError("Could not update votes, try again later.");
+    });
+  };
+
+  const handleDownvote = () => {
+    const voteChange = downvote ? 1 : upvote ? -2 : -1;
+    setDownvote(!downvote);
+    setUpvote(false);
+    setCurrArticle({ ...currArticle, votes: currArticle.votes + voteChange });
+    patchArticleVotes(article_id, voteChange).catch((err) => {
+      setCurrArticle({ ...currArticle, votes: currArticle.votes - voteChange });
+      setCommentError("Could not update votes, try again later.");
+    });
+  };
 
   useEffect(() => {
     getArticleByID(article_id)
@@ -29,9 +58,12 @@ export default function ArticleInfo() {
       });
   }, [article_id]);
 
-  if (loading) return <p>Loading..</p>;
-  if (error) return <p>{error}</p>;
-
+  if (loading) {
+    return <p>Loading..</p>;
+  }
+  if (error) {
+    return <p>{error}</p>;
+  }
   return (
     <div>
       <section className="mx-auto p-4 bg-white shadow-md rounded-lg flex flex-row w-4/5">
@@ -43,7 +75,28 @@ export default function ArticleInfo() {
           />
         </div>
         <div>
-          <h2 className="text-2xl font-bold mb-2">{currArticle.title}</h2>
+          <div className="flex flex-row justify-between">
+            <h2 className="text-2xl font-bold mb-2">{currArticle.title}</h2>
+            <div className="flex flex-col w-max">
+              <button
+                className={`font-extrabold text-5xl ${
+                  upvote ? "text-red-500" : ""
+                }`}
+                onClick={handleUpvote}
+              >
+                +
+              </button>
+              <button
+                className={`font-extrabold text-5xl ${
+                  downvote ? "text-blue-500" : ""
+                }`}
+                onClick={handleDownvote}
+              >
+                -
+              </button>
+              {commentError ? <p>{commentError}</p> : null}
+            </div>
+          </div>
           <p className="text-gray-600 mb-2">
             <strong>Votes:</strong> {currArticle.votes}
           </p>
