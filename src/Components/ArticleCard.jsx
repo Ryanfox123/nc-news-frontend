fc0e7962970ed283ad608a266c006d269df192bf
Ryan Fox <ryanfox1@hotmail.co.uk>
2024-11-05 08:56:13 +0000

    added links to article titles

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
 export default function ArticleCard({ article }) {
   return (
     <div>
-      <h3>{article.title}</h3>
+      <h3 className="underline hover:text-sky-700">
+        <Link to={`/article/${article.article_id}`}>{article.title}</Link>
+      </h3>
       <p>{article.topic}</p>
       <img className="size-24" src={article.article_img_url} />
       <p>Created by: {article.author}</p>
